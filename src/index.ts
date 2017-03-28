11018a91587fc6f3166196dbdaad342267a78dd5
Michael Simmons <github@e7i.org>
2017-03-27 22:13:32 -0500

    Deleted unnecessary barrel

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './configuration';
-
-export * from './billing.service';
-
-export * from './wizard/wizard.component';
-export * from './plans/plans.component';
-export * from './payment/payment.component';
