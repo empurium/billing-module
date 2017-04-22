a6361147a9a2eb7bb173e36213d72a5ddc06da9d
Michael Simmons <github@e7i.org>
2017-04-22 18:26:33 -0500

    BillingButton: router navigate to open billing modal

--- src/billing-button/billing-button.component.ts
+++ src/billing-button/billing-button.component.ts
@@ -1,30 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { StripeService } from '../+services/stripe.service';
-import { ModalService } from '../+services/modal.service';
 
 
 @Component({
-    selector: 'studio-billing-button',
-    template: `<button (click)="open()" [disabled]="disabled" class="btn btn-secondary btn-pointer">{{ label }}</button>`,
-    styleUrls: ['./billing-button.component.scss'],
+    selector:    'studio-billing-button',
+    templateUrl: './billing-button.component.html',
+    styleUrls:   ['./billing-button.component.scss'],
 })
 export class BillingButtonComponent implements OnInit {
     public disabled: boolean = true;
-    public label: string = 'Loading...';
+    public label: string     = 'Loading...';
 
-    constructor(private stripe: StripeService,
-                private modalService: ModalService) {
+    constructor(private router: Router,
+                private stripe: StripeService) {
     }
 
     public ngOnInit(): void {
         this.stripe.configure(() => {
             this.disabled = false;
-            this.label = 'Manage Billing';
+            this.label    = 'Manage Billing';
         });
     }
 
     public open(): void {
-        this.modalService.events.emit('show');
+        this.router.navigate([], {
+            queryParams: {
+                module: 'billing',
+                step:   'plans',
+            },
+        });
     }
 }
