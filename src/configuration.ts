8e35641e3921e48e53c909ad46896b36512d911b
Michael Simmons <gitlab@e7i.org>
2018-07-12 18:46:20 +0800

    Remove dead ID

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,6 +1,6 @@
 // Hardcoded Gateway ID as we only anticipate one Stripe account
 // We can move this to a configuration option if that changes
-export const gatewayId: string = 'e4db7e5c-3cc6-4bf9-8274-60e30f605c7a';
+export const gatewayId: string = 'xyz';
 
 export const stripeStyles: any = {
     base: {
